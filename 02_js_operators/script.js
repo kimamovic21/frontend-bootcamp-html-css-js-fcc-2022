4b709dcb906182fb66b42dbd12a0544b0a069957
kimamovic21 <imamovic.kerim@gmail.com>
2022-10-27 17:00:08 +0200

    izmjene
    -02 folder
    -03 folder

--- 02_js_operators/script.js
+++ 02_js_operators/script.js
@@ -25,4 +25,82 @@
 // console.log(startingNumber);
 
 
-// video 2:00:00
+
+// let x = 10;
+// x = x + 5;
+// console.log(x);
+
+
+// let x = 10;
+// x += 5;
+// console.log(x);
+
+
+
+// Example
+// const firstArray = [1,2,3,4,5];
+// const secondArray = [1,2,3,4,5];
+
+// console.log(firstArray === firstArray);
+// console.log(secondArray === secondArray);
+// console.log(firstArray === secondArray);
+
+
+// Example
+// const firstObject = {
+//     name: 'Kerim',
+//     surname: 'Imamovic',
+// };
+
+// const secondObject = {
+//     name : 'Kerim',
+//     surname: 'Imamovic',
+// };
+
+// console.log(firstObject === firstObject);
+// console.log(secondObject === secondObject);
+// console.log(firstObject === secondObject);
+
+
+
+// Example
+// const result = 20 === 20;
+// console.log(result ? 'values match' : 'values do not match');
+
+
+
+// Example
+// const isUserLoggedIn = true;
+// const doesUserHavePermission = false;
+// const canUserPerformAction = isUserLoggedIn && doesUserHavePermission;
+// console.log(canUserPerformAction);
+
+// const isUserLoggedIn2 = true;
+// const doesUserHavePremiumAccount = false;
+// const canUserPerformAction2 = isUserLoggedIn2 || doesUserHavePremiumAccount;
+// console.log(canUserPerformAction2);
+
+
+
+// Example
+const myObj = {
+    nestedObject1: {
+        price: 100,
+        quantity: 5,
+    },
+    nestedObject2: {
+        price: 150,
+        quantity: 2,
+    },
+};
+
+const myArray = [myObj.nestedObject1, myObj.nestedObject2];
+console.log(myArray);
+
+const result1 = myArray[0].price * myArray[0].quantity;
+console.log(result1);
+const result2 = myArray[1].price * myArray[1].quantity;
+console.log(result2);
+
+const totalResult = result1 > result2;
+console.log(totalResult);
---
+++ 03_functions_conditionals_loops/script.js
@@ -0,0 +1,154 @@
+// Loops in javascript
+
+// const blogPosts = [
+//     {
+//         title: 'Title 1',
+//         author: 'Name1 and surname1',
+//         publishDate: '01.01.2001.'
+//     },
+//     {
+//         title: 'Title 2',
+//         author: 'Name2 and surname2',
+//         publishDate: '02.02.2002.'
+//     },
+//     {
+//         title: 'Title 3',
+//         author: 'Name3 and surname3',
+//         publishDate: '03.03.2003.'
+//     },
+// ];
+
+// Manual way
+// const postTitle = blogPosts[0].title;
+// const postAuthor = blogPosts[0].author;
+// const postPublishDate = blogPosts[0].publishDate;
+
+// console.log(postTitle);
+// console.log(postAuthor);
+// console.log(postPublishDate);
+
+// For loop
+// for (let i = 0; i < blogPosts.length; i++) {
+
+//     const postTitle = blogPosts[i].title;
+//     const postAuthor = blogPosts[i].author;
+//     const postPublishDate = blogPosts[i].publishDate;
+    
+//     console.log(postTitle);
+//     console.log(postAuthor);
+//     console.log(postPublishDate);
+// };
+
+
+
+// Loops example
+// for (let i = 0; i <= 10; i++) {
+//     console.log(i);
+// };
+
+
+
+// Loops example
+// const arr = ['Sarajevo', 'Tuzla', 'Banja Luka', 1, 2, 3, 4, 5, 'Zenica', 'Mostar', 'Bihac', 'Gorazde'];
+// console.log(arr);
+
+// // for (let i = 0; i < arr.length; i++) {
+// //     console.log(arr.length);
+// // };
+
+// for (let i = 0; i < arr.length; i++) {
+//     if (typeof arr[i] === 'number') {
+//         console.log(arr[i]);
+//     };
+// };
+// loop through an entire array, look at the values, see if it is a number, if it is, it will print them to the console
+
+
+
+
+// Functions in Javascript
+// function myFunction() {
+//     console.log('Hello world');
+// };
+// myFunction();
+
+
+
+// Parameters and arguments
+// function myFunction(param1, param2) {
+//     console.log(param1);
+//     console.log(param2);
+// };
+// myFunction(1,2);
+
+
+
+// Scopes in javascript
+// function myFunction() {
+//     let result = 'Hello world';
+//     console.log(result);
+// }
+// myFunction();
+
+
+
+// Arrow functions
+// const arrowFunction = () => {
+//     console.log('I am an arrow function.');
+// };
+// arrowFunction();
+
+
+
+// return values in functions
+// const returnFunction = () => {
+//     const a = 20;
+//     return a > 30;
+// };
+// const result = returnFunction();
+// console.log(result);
+
+
+// Example
+// const myFunction = () => {
+//     let myNumber = 20;
+//     return myNumber;
+//     myNumber = 50;
+// }
+// console.log(myFunction());
+
+
+
+// Example
+// const myNumber = 20;
+// let myFunction = () => {
+//     if(myNumber < 50) {
+//         return 'Returned the function early';
+//     };
+//     return 40;
+// };
+// console.log(myFunction());
+
+
+
+// Example
+// function myFunction() {
+//     return 20;
+// }
+// console.log(myFunction());
+
+// const aliasVariable = myFunction;
+// console.log(aliasVariable());
+
+// const myObj = {
+//     prop1: 50,
+//     prop2: myFunction,
+// };
+// console.log(myObj);
+
+// console.log(myObj.prop2);
+
+
+
+// Javascript built-in functions
+// video 3:45:00
